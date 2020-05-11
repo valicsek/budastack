bf09f62d4802def384e8673d153ebb68e0ade8bf
medve <entriek@gmail.com>
2020-05-11 15:06:42 +0200

    fix padding and heading issue on small devices

--- index.vue
+++ index.vue
@@ -8,7 +8,7 @@
           button.bg-blue-500.text-white.font-bold.shadow-md.rounded.px-12.py-5.mr-5(class="w-full lg:w-auto") Show your works
           button.border.font-bold.rounded.px-12.py-5(class="w-full lg:w-auto mt-2 lg:mt-0") I want to work with you
       section.px-5(class="lg:py-10 lg:px-0")
-        h2.max-w-5xl.mx-auto.text-6xl.mb-10.text-center Technologies I Use
+        h2.max-w-5xl.mx-auto.text-4xl.mb-10.text-center(class="lg:text-6xl") Technologies I Use
         .flex.overflow-x-scroll
           img.w-24.h-24.mr-10(loading="lazy" src='https://miro.medium.com/max/800/1*Pk2mZo1cBqfVqQi-mtAkuA.png' alt='vue')
           img.w-24.h-24.mr-10(loading="lazy" src='https://webpack.js.org/dcd5e077cf9f54ebe52d4f7ebe8c3080.png' alt='webpack')
@@ -33,9 +33,9 @@
           img(loading="lazy" src="/images/measurement.png")
       section.bg-black.py-20
         div.max-w-5xl.mx-auto.py-5.px-5(class="lg:px-0")
-          h2.text-6xl.mb-5.text-white.text-center Maybe you might be interested
+          h2.text-4xl.mb-5.text-white.text-center(class="lg:text-6xl") Maybe you might be interested
           p.text-2xl.text-center.font-thin.my-5.text-white I like to take notes and share my knowledge. You may find some relevant information which helps you to decide.
-          .p-2.grid.grid-cols-1.grid-rows-1.grid-flow-row.gap-2(class="sm:grid-cols-2 lg:grid-cols-3")
+          .grid.grid-cols-1.grid-rows-1.grid-flow-row.gap-2(class="sm:grid-cols-2 lg:grid-cols-3")
             Card(
               v-for="blog of $site.pages.filter(i => i.regularPath.includes('/blog/'))"
               :tag='blog.frontmatter.tag'
